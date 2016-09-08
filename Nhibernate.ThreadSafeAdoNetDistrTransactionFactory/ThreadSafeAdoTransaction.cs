4a24f907d1a653b8de47d35be339e4ac7a2b1c83
podzyuban <podzyuban@gmail.com>
2016-09-08 12:12:16 +0300

    init commit

---
+++ Nhibernate.ThreadSafeAdoNetDistrTransactionFactory/ThreadSafeAdoTransaction.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Data;
+using NHibernate;
+using NHibernate.Transaction;
+
+namespace Nhibernate.ThreadSafeAdoNetDistrTransactionFactory
+{
+    internal class ThreadSafeAdoTransaction : ITransaction
+    {
+        private readonly ITransaction _source;
+        private readonly object _lock;
+
+        public ThreadSafeAdoTransaction(ITransaction source, object @lock)
+        {
+            this._source = source;
+            this._lock = @lock;
+        }
+
+        public void Dispose()
+        {
+            this.WithLock(this._source.Dispose);
+        }
+
+        public void Begin()
+        {
+            this._source.Begin(); // not nessary lock sync
+        }
+
+        public void Commit()
+        {
+            this.WithLock(this._source.Commit);
+        }
+
+        public void Rollback()
+        {
+            this.WithLock(this._source.Rollback);
+        }
+
+        public bool IsActive => this._source.IsActive;
+
+        public bool WasRolledBack
+        {
+            get { return this.WithLock(() => this._source.WasRolledBack); }
+        }
+
+        public bool WasCommitted
+        {
+            get { return this.WithLock(() => this._source.WasCommitted); }
+        }
+        public void RegisterSynchronization(ISynchronization synchronization)
+        {
+            this.WithLock(() => this._source.RegisterSynchronization(synchronization));
+        }
+
+        public void Enlist(IDbCommand command)
+        {
+            this._source.Enlist(command); // not nessary lock sync
+        }
+
+        public void Begin(System.Data.IsolationLevel isolationLevel)
+        {
+            this._source.Begin(isolationLevel);
+        }
+
+        private void WithLock(Action action)
+        {
+            WithLock<object>(() =>
+            {
+                action();
+                return null;
+            });
+        }
+
+        private T WithLock<T>(Func<T> func)
+        {
+            lock (this._lock)
+            {
+                var result = func();
+
+                return result;
+            }
+        }
+    }
+}
